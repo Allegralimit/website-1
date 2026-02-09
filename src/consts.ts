4ff548e9f9b77abd8e53001871b7236d6260649f
Allegralimit <meandmayu1@gmail.com>
2026-02-09 17:53:47 -0600

    Customize site: 5 pages, clean minimal design

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Mayukhee Das";
+export const SITE_DESCRIPTION = "Welcome to my corner of the internet.";
